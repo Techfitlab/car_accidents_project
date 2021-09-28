cf99c55eb040b930b5b83ac39db249b937481d38
Techfitlab <33873356+Techfitlab@users.noreply.github.com>
2021-09-28 13:14:55 +0100

    Add files via upload

---
+++ bin/model_run_script.sh
@@ -0,0 +1,25 @@
+# Activate virtual environment
+source "/home/airflow3/bin/activate"
+DIR="/Users/greengodfitness/Desktop/Vodafone"
+LOGFILE="$DIR/log"
+
+# Log Details
+TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+echo "$TIMESTAMP - Starting BASH script ->" >> $LOGFILE/bash.log
+echo "$TIMESTAMP - Starting BASH script ->"
+
+# Run the script
+TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+echo "$TIMESTAMP - Running Python Service (ETL) ->" >> $LOGFILE/bash.log
+echo "$TIMESTAMP - Running Python Service (ETL) ->"
+
+echo " "
+echo " "
+
+cd "$DIR"
+/home/airflow3/bin/python $DIR/src/model.py
+
+# Log Details
+TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+echo "$TIMESTAMP - Finishing BASH script..." >> $LOGFILE/bash.log
+echo "$TIMESTAMP - Finishing BASH script..."
